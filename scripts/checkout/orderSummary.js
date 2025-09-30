b349bf42c6cd0d68bf2691bea8529ee2a5b74948
Akshay-Kumar-Mishra-00-1-1 <radhikaradhika9720@gmail.com>
2025-10-01 04:12:19 +0530

    Make payment summary interactive.

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,10 +3,7 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js'; 
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions, getDeliveryOptions } from '../../data/deliveryOptions.js';
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-deliveryDate.format('dddd, MMMM D');
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -118,6 +115,8 @@ document.querySelectorAll('.js-delete-link')
 
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
+
+      renderPaymentSummary();
     });
   });
 
@@ -127,6 +126,7 @@ document.querySelectorAll('.js-delivery-option')
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }
